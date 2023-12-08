f2ff4053a1698312611449f0163a18cba752d1eb
Leonardo Perdomo <leonardop763@gmail.com>
2023-12-08 14:59:00 -0500

    Feat: Loader added when updating the profile.

--- auth-frontend/src/pages/EditInfo/EditInfo.jsx
+++ auth-frontend/src/pages/EditInfo/EditInfo.jsx
@@ -7,31 +7,37 @@ import Navbar from '../../components/Navbar/Navbar';
 import EditProfileForm from '../../components/EditProfileForm/EditProfileForm';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import './EditInfo.css';
+import Loader from '../../components/Loader/Loader';
 
 export default function EditInfo() {
 	const [errorMessage, setErrorMessage] = useState(null);
 	const { isDarkMode } = useColorMode();
 	const navigate = useNavigate();
 	const { user, editUser } = useUser();
-	
-	
+	const [isLoading, setIsLoading] = useState(false);
+
 	const handleSubmit = async (values) => {
-		await updateUser(values).then((updatedUser) => {
-			editUser(updatedUser);
-			navigate('/personal-info');
-		}).catch((error) => {
-			setErrorMessage("You must enter the correct password to update your information");
-		});
+		// Show loader
+		setIsLoading(true);
+		await updateUser(values)
+			.then((updatedUser) => {
+				editUser(updatedUser);
+				navigate('/personal-info');
+			})
+			.catch((error) => {
+				setErrorMessage('You must enter the correct password to update your information');
+			})
+			.finally(() => {
+				// Hide loader
+				setIsLoading(false);
+			});
 	};
 
 	return (
 		<>
-			<Navbar user={user}/>
+			<Navbar user={user} />
 			<div className='edit-info__back-container'>
-				<Link
-					to='/personal-info'
-					className={`edit-info__back ${isDarkMode}`}
-				>
+				<Link to='/personal-info' className={`edit-info__back ${isDarkMode}`}>
 					<ArrowBackIosIcon />
 					Back
 				</Link>
@@ -39,15 +45,14 @@ export default function EditInfo() {
 
 			<div className='container edit-container'>
 				<main className='edit-info'>
-					<h1 className={`edit-info__title ${isDarkMode}`}>
-						Change Info
-					</h1>
-					<p className={`edit-info__text ${isDarkMode}`}>
-						Changes will be reflected to every services
-					</p>
+					<h1 className={`edit-info__title ${isDarkMode}`}>Change Info</h1>
+					<p className={`edit-info__text ${isDarkMode}`}>Changes will be reflected to every services</p>
 				</main>
 				<EditProfileForm onSubmit={handleSubmit} user={user} error={errorMessage} />
 			</div>
+			{isLoading ? (
+				<Loader />
+			) : null}
 		</>
 	);
 }
