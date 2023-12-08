1190ad23b3c5cb09055844a18fa5958dd789fcc7
Leonardo Perdomo <leonardop763@gmail.com>
2023-12-08 03:46:27 -0500

    Changed: Capitalized images file names.

--- auth-frontend/src/constants/images.js
+++ auth-frontend/src/constants/images.js
@@ -1,9 +1,9 @@
 import logo from "../assets/devchallenges.svg";
 import logoDark from "../assets/devchallenges-light.svg";
-import facebookIcon from "../assets/facebook.svg";
-import githubIcon from "../assets/github.svg";
-import googleIcon from "../assets/google.svg";
-import twitterIcon from "../assets/twitter.svg";
+import facebookIcon from "../assets/Facebook.svg";
+import githubIcon from "../assets/Github.svg";
+import googleIcon from "../assets/Google.svg";
+import twitterIcon from "../assets/Twitter.svg";
 import user from "../assets/user.png";
 
 const images = {
